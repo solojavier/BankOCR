144c002b09754e64b7e91358d8b0ce7fda4c3443
Javier Cervantes <javier@hybridgroup.com>
2014-07-22 12:49:35 -0500

    Making first user story pass
    Each entry is 4 lines long, and each line has 27 characters. The first
    3 lines of each entry contain an account number written using pipes and
    underscores, and the fourth line is blank.
    
    Each account number should have 9 digits, all of which should be in the
    range 0-9.

--- lib/utils.rb
+++ lib/utils.rb
@@ -2,8 +2,40 @@ module BankOCR
   module Utils
 
     def parse_file(input_path)
-      []
+      file    = File.open(input_path)
+      numbers = []
+      conversions = {
+        " _ | ||_|" => "0",
+        "     |  |" => "1",
+        " _  _||_ " => "2",
+        " _  _| _|" => "3",
+        "   |_|  |" => "4",
+        " _ |_  _|" => "5",
+        " _ |_ |_|" => "6",
+        " _   |  |" => "7",
+        " _ |_||_|" => "8",
+        " _ |_| _|" => "9"
+      }
+
+      while !file.eof?
+        top    = file.readline
+        middle = file.readline
+        bottom = file.readline
+        _blank = file.readline
+        numbers << (0..8).map do |i|
+          low  = (i * 3)
+          high = ((i + 1) * 3) - 1
+          shape = top[low..high] + middle[low..high] + bottom[low..high]
+          conversions[shape]
+        end.join
+
+      end
+
+      numbers
+    ensure
+      file.close
     end
 
   end
+
 end
