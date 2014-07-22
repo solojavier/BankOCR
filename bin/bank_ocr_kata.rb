61e53c792d92be8a910630abc1eae855c4b49462
Javier Cervantes <javier@hybridgroup.com>
2014-07-22 15:23:48 -0500

    Removing application object

--- bin/bank_ocr_kata.rb
+++ bin/bank_ocr_kata.rb
@@ -1,7 +1,9 @@
-require_relative '../application'
+require_relative '../lib/utils'
 
 INPUT  = File.dirname(__FILE__) + '/input/ocr_input.in'
 OUTPUT = File.dirname(__FILE__) + '/output/ocr_accounts_report.out'
 
-BankOCR::Application.new(INPUT, OUTPUT).run
+include BankOCR::Utils
+
+generate_report(OUTPUT, validate_accounts(parse_file(INPUT)))
 p "OCR accounts report generated succesfully"
--- spec/bin/bank_orc_kata_spec.rb
+++ spec/bin/bank_orc_kata_spec.rb
@@ -1,6 +1,4 @@
-require_relative '../application'
-
-describe BankOCR::Application do
+describe "Application execution" do
 
   let(:input)  { File.dirname(__FILE__) + '/ocr_input.in' }
   let(:output) { File.dirname(__FILE__) + '/accounts_report.out' }
@@ -9,10 +7,13 @@ describe BankOCR::Application do
     "711111111\n777777177\n200800000\n333393333\n888888888 ERR\n555555555 ERR\n666666666 ERR\n999999999 ERR\n490067715 ERR\n123456789\n000000051\n49086771? ILL\n"
   end
 
-  subject { described_class.new(input, output) }
+  subject { Object.new.extend(BankOCR::Utils) }
 
   it 'parses file and creates report' do
-    subject.run
+    account_numbers = subject.parse_file(input)
+    accounts        = subject.validate_accounts(account_numbers)
+
+    subject.generate_report(output, accounts)
 
     expect(File.open(output).read).to eq(report)
   end
